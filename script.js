1647760e243c23798f579bafc16cd2d19cf90f5a
Valentyn <143123781+VaL2111@users.noreply.github.com>
2025-09-17 18:47:46 +0300

    Implement functionality of main and secondary buttons for the first practical task

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const buttons = document.querySelectorAll(".work-btn");
+const mainButtons = document.querySelectorAll(".main-btn");
 const contents = document.querySelectorAll(".work-content");
+const secondaryButtons = document.querySelectorAll(".second-btn");
+const contentBoxes = document.querySelectorAll(".answer-box");
 
-buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
+mainButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const targetId = button.getAttribute("data-target");
+        const target = document.getElementById(targetId);
+
         contents.forEach(c => c.classList.remove("active"));
-        const target = document.getElementById(btn.dataset.target);
         target.classList.add("active");
+    });
+});
+
+secondaryButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const contentId = button.getAttribute("data-content");
+        const content = document.getElementById(contentId);
+
+        contentBoxes.forEach(c => c.classList.remove("active"));
+        content.classList.add("active");
     });
 });
